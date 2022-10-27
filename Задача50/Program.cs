588f7d8553ff61ecef5a5805628a46712924a23c
Alexander Prilepin <aleksander930@list.ru>
2022-10-28 01:51:03 +0300

    Lesson7

---
+++ Задача50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int[,] numarr(int a, int b, int min, int max)
+{
+    int[,] array = new int[a, b];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],4}, ");
+            else Console.Write($"{arr[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+int arraynum(int[,] matrix, int indexX, int indexY)
+{
+    int elemValue = matrix[indexX, indexY];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (indexX < matrix.GetLength(0) && indexY < matrix.GetLength(1)) return elemValue;
+        }
+    }
+    return elemValue;
+}
+int[,] array2D = numarr(3, 4, -100, 100);
+Print(array2D);
+Console.WriteLine();
+
+if (num1 < array2D.GetLength(0) && num2 < array2D.GetLength(1))
+{
+    int indexnum = arraynum(array2D, num1, num2);
+    Console.WriteLine(indexnum);
+}
+else Console.WriteLine("Введите корректные значения");
---
+++ Задача52/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{Math.Round(array[i], 1, MidpointRounding.ToEven)}, ");
+        else Console.Write($"{Math.Round(array[i], 1, MidpointRounding.ToEven)}");
+    }
+}
+
+int[,] RndInt(int a, int b, int min, int max)
+{
+    int[,] arr = new int[a, b];
+    var rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void Print(int[,] num)
+{
+    for (int i = 0; i < num.GetLength(0); i++)
+
+    {
+        Console.Write("|");
+        for (int j = 0; j < num.GetLength(1); j++)
+        {
+            if (j < num.GetLength(1) - 1) Console.Write($"{num[i, j],4}, ");
+            else Console.Write($"{num[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+double[] Arrnumarey(int[,] numbers)
+{
+    double[] array = new double[numbers.GetLength(1)];
+    int ind = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double variable = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+
+            variable = (variable + numbers[i, j]);
+
+        }
+        variable = variable / numbers.GetLength(0);
+        array[ind] = variable;
+        ind++;
+    }
+    return array;
+}
+
+int[,] anumbers = RndInt(3, 4, 0, 9);
+Print(anumbers);
+Console.WriteLine();
+double[] arrayA = Arrnumarey(anumbers);
+System.Console.Write("Ответ: ");
+PrintArray(arrayA);
